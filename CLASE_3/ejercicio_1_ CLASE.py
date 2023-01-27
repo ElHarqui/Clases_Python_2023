6c78cf7f8b6c070f208d7af12435bd0af8a96720
ElHarqui <sebastian.peralta@unmsm.edu.pe>
2023-01-27 12:55:16 -0500

    TERMINADO SEMANA 3

---
+++ CLASE_3/ejercicio_1_ CLASE.py
@@ -0,0 +1,16 @@
+class restaurante( ): 
+  def __init__(self,name = 'HOLS',cocina = 'ADIO'):
+    self.restaurant_name =  name
+    self.cuisine_name = cocina
+  def describe_restaurant(self):
+    return f"El nombre del restaurante es {self.restaurant_name} y su tipo de cocina es {self.cuisine_name}"
+  #def open_restaurant(self):
+    
+try:
+  gaaa = restaurante()
+  print(gaaa.describe_restaurant())
+  #print(gaaa)
+except Exception as a:
+  print(a)
+  print("ERROR")
+  #= "SinNombre"
